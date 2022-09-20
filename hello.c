1e35dbbff457896c8538837cb931ed02a333f6c1
GarrettJenkins3 <gjenkins3@huskers.unl.edu>
2022-09-20 08:43:30 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+Author: Garrett Jenkins
+Date: 8/23/2022
+Hack 1.0
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Hello World!\n");
+    printf("Garrett Jenkins\n");
+
+    return 0;
+}
